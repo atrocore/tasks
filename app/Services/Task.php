04fc3563bab8ef6f4ae64246cfba1ef55390d657
Roman Zablodskyi <r.zablodskyi@gmail.com>
2021-04-29 15:54:08 +0300

    Fixed Bad Response in Task

--- app/Services/Task.php
+++ app/Services/Task.php
@@ -33,22 +33,9 @@
 
 namespace ActivitiesTasks\Services;
 
-use \Espo\Core\Exceptions\Error;
-use \Espo\Core\Exceptions\Forbidden;
+use Espo\Services\Record;
 
-use \Espo\ORM\Entity;
-
-class Task extends \Espo\Services\Record
+class Task extends Record
 {
-    public function loadAdditionalFields(Entity $entity)
-    {
-        parent::loadAdditionalFields($entity);
-        $this->loadRemindersField($entity);
-    }
 
-    protected function loadRemindersField(Entity $entity)
-    {
-        $reminders = $this->getRepository()->getEntityReminderList($entity);
-        $entity->set('reminders', $reminders);
-    }
 }
